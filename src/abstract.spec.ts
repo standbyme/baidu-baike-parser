51d79fa431b0dc4989a8e14eeb568a9c20926af0
TGS <tgs@tgs.blue>
2018-02-11 10:38:54 +0800

    fix bug

--- src/abstract.spec.ts
+++ src/abstract.spec.ts
@@ -18,7 +18,7 @@ describe('Abstract Parser', function () {
     it('Return Some if it exists', async function () {
         const mock_url = 'https://baike.baidu.com/item/%E7%8E%8B%E6%AF%85/2652548'
         const html = await get_html(mock_url)
-        const { abstract } = abstract_parser(html).get()
+        const abstract = abstract_parser(html).get()
         const text = `王毅，男，北京人`
         assert.equal(abstract.slice(0, 8), text)
     })
--- src/abstract.ts
+++ src/abstract.ts
@@ -1,13 +1,13 @@
 import { Option } from 'funfix-core'
 
-export default function abstract_extractor(original_html: CheerioStatic): Option<{ abstract: string }> {
+export default function (original_html: CheerioStatic): Option<string> {
     const found = original_html('.lemma-summary')
     if (found.length) {
         return Option.of(found).map(
             m => {
                 const text = m.text()
                 const abstract = text.slice(1, text.length - 1)
-                return { abstract }
+                return abstract
             }
         )
     } else return Option.none()
